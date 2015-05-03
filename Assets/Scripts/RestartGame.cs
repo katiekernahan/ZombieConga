84f285c6c29fa40a0c8fe01cbe43b787ebc99ea1
katiekernahan <katiekernahan@gmail.com>
2015-05-03 15:27:36 +1000

    Actual Game
    Game Files

---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class RestartGame : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		Invoke("LoadLevel", 3f);
+	}
+	
+	void OnGUI()
+	{
+		if (GUI.Button (new Rect (Screen.width / 2 - 20, 10, 80, 20), "RESTART")) {
+			Application.LoadLevel ("CongaScene");
+		}
+	}
+}
---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke("LoadLevel", 3f);
+	}
+
+	void OnGUI()
+	{
+		if (GUI.Button (new Rect (Screen.width / 2 - 20, 10, 80, 20), "START")) {
+			Application.LoadLevel ("CongaScene");
+		}
+	}
+}
